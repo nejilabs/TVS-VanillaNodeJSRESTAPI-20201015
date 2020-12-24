d3429bcd10aa47ffa862c242ec7f36bcd613a207
nellyXinwei <nelsonalbajr@yahoo.com>
2020-12-25 01:51:15 +0800

    mvc and get all products

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const http = require('http');
-const products = require('./data/products');
+const {getProducts} = require('./controllers/productsControllers');
 
 const server = http.createServer((req,res)=>{
   if(req.url === '/'){
     res.writeHead(200,{'Content-Type':'text/html'});
     res.end('<h1>Home</h1>');
   }else if(req.url === '/api/products' && req.method === "GET"){
-    res.writeHead(200,{'Content-Type':'application/json'});
-    res.end(JSON.stringify(products));
+    getProducts(req,res)
   }else{
     res.writeHead(404,{'Content-Type':'application/json'});
     res.end(JSON.stringify({message:'Route Not Found'}));
