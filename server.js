b2cc06ae377b9b7514ef9a80407de16bb8d7b9a4
nellyXinwei <nelsonalbajr@yahoo.com>
2020-12-25 01:18:28 +0800

    Getting Data from JSON and simple routing

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const http = require('http');
+const products = require('./data/products');
 
 const server = http.createServer((req,res)=>{
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<h1>Hello World</h1>');
-  res.end();
-})
+  if(req.url === '/'){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end('<h1>Home</h1>');
+  }else if(req.url === '/api/products' && req.method === "GET"){
+    res.writeHead(200,{'Content-Type':'application/json'});
+    res.end(JSON.stringify(products));
+  }else{
+    res.writeHead(404,{'Content-Type':'application/json'});
+    res.end(JSON.stringify({message:'Route Not Found'}));
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 
